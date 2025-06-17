b58118dba89a27069544914259ee940419325846
Kars van Velzen <107920501+JeCheeseSmith@users.noreply.github.com>
2025-06-17 23:38:16 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ function App() {
 
           <div className="buttonGroup">
               <ButtonGroup
-                  buttons={[["Home", "https://home.octubre.be"], ["Cloud", "https://cloud.octubre.be"], ["Me", "https://me.octubre.be"], ["Chat", "https://chat.octubre.be"], ["Archive", "https://archive.octubre.be"], ["Dev", "https://dev.octubre.be"], ["Blog", "https://blog.octubre.be"], ["Status", "https://status.octubre.be"], ["Log", "https://log.octubre.be"], ["Media", "https://media.octubre.be"]]}/>
+                  buttons={[["Home", "https://home.octubre.be"], ["Cloud", "https://cloud.octubre.be"], ["Me", "https://me.octubre.be"], ["Chat", "https://chat.octubre.be"], ["Archive", "https://archive.octubre.be"], ["Dev", "https://dev.octubre.be"], ["Blog", "https://blog.octubre.be"], ["Status", "https://status.octubre.be"], ["Log", "https://log.octubre.be"], ["Media", "https://media.octubre.be"], ["Pyros", "https://pyros.octubre.be"] ]}/>
           </div>
 
           <h3>Contact</h3>
